2fb7c727f4fa025ff0f3cbbc2fdbd31a0b7fdd6b
Igor Lira <igorlira@live.com>
2021-03-23 18:50:57 -0700

    make `pickMask` open

--- inputmask/src/main/kotlin/com/redmadrobot/inputmask/MaskedTextChangedListener.kt
+++ inputmask/src/main/kotlin/com/redmadrobot/inputmask/MaskedTextChangedListener.kt
@@ -239,7 +239,7 @@ open class MaskedTextChangedListener(
         }
     }
 
-    private fun pickMask(
+    open fun pickMask(
         text: CaretString
     ): Mask {
         if (this.affineFormats.isEmpty()) return this.primaryMask
